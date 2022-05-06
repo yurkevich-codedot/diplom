17028455e85b6b5cb6af9f12ab3ef31a4aad5065
DaniilYurkevich <daniilsergeevich200111@gmail.com>
2022-05-06 23:18:50 +0300

    fix&

--- src/js/includes/block-menu.js
+++ src/js/includes/block-menu.js
@@ -41,20 +41,17 @@ const swiper = new Swiper(".swiper", {
 document.querySelector(".header__menu-btn").addEventListener("click", (e) => {
   if (!e.target.classList.contains("lock-scroll")) {
     document.querySelector("body").classList.add("lock-scroll");
-    console.log("no");
   }
 });
 
 document.querySelector(".backdrop").addEventListener("click", (e) => {
   if (!e.target.classList.contains("lock-scroll")) {
     document.querySelector("body").classList.remove("lock-scroll");
-    console.log("no");
   }
 });
 
 document.querySelector(".header__cross").addEventListener("click", (e) => {
   if (!e.target.classList.contains("lock-scroll")) {
     document.querySelector("body").classList.remove("lock-scroll");
-    console.log("no");
   }
 });
