be804fe28acaecb24377c7272a8a77377d93c586
DaniilYurkevich <daniilsergeevich200111@gmail.com>
2022-05-07 16:52:16 +0300

    made mapbox and customize

--- src/js/includes/block-menu.js
+++ src/js/includes/block-menu.js
@@ -1,10 +1,14 @@
 const { Navigation } = require("swiper");
 
 import Swiper from "swiper";
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGFudmVyeXVyayIsImEiOiJjbDJ1NXY3YWgwYjV3M2NvNHRteW9tZXpkIn0.zXeb65io4SiZQl3BbejBMQ";
+
 const swiper = new Swiper(".swiper", {
   centeredSlides: false,
   modules: [Navigation],
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 10,
 
   pagination: {
@@ -20,10 +24,10 @@ const swiper = new Swiper(".swiper", {
   },
 
   breakpoints: {
-    420: {
+    0: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 20,
+      spaceBetween: 10,
     },
     768: {
       slidesPerView: 3,
@@ -31,11 +35,72 @@ const swiper = new Swiper(".swiper", {
       spaceBetween: 40,
     },
     1280: {
-      slidesPerView: 4,
+      slidesPerView: 5,
       slidesPerGroup: 4,
       spaceBetween: 50,
     },
   },
+});
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/danveryurk/cl2vsxthg000114owcquvgewl",
+  center: [30.202, 55.185],
+  zoom: 12,
+});
+
+map.on("load", () => {
+  // Add an image to use as a custom marker
+  map.loadImage("https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png", (error, image) => {
+    if (error) throw error;
+    map.addImage("custom-marker", image);
+    // Add a GeoJSON source with 2 points
+    map.addSource("points", {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            // feature for Mapbox SF
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [30.198897, 55.193295],
+            },
+            properties: {
+              title: "Кировский мост",
+            },
+          },
+          {
+            // feature for Mapbox SF
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [30.2006252, 55.1845255],
+            },
+            properties: {
+              title: "Три штыка",
+            },
+          },
+        ],
+      },
+    });
+
+    // Add a symbol layer
+    map.addLayer({
+      id: "points",
+      type: "symbol",
+      source: "points",
+      layout: {
+        "icon-image": "custom-marker",
+        // get the title name from the source's "title" property
+        "text-field": ["get", "title"],
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [0, 1.0],
+        "text-anchor": "top",
+      },
+    });
+  });
 });
 
 document.querySelector(".header__menu-btn").addEventListener("click", (e) => {
@@ -55,3 +120,19 @@ document.querySelector(".header__cross").addEventListener("click", (e) => {
     document.querySelector("body").classList.remove("lock-scroll");
   }
 });
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+header.classList.add("sticky");
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
